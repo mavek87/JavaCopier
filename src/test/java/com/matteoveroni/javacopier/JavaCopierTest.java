7d958535a5baeec7f4ecd9b9fa5dc27d963261c8
Matteo Veroni <matver87@gmail.com>
2019-02-25 15:39:40 +0100

    add test also for multi copy from a srcDir to a destDir

--- src/test/java/com/matteoveroni/javacopier/JavaCopierTest.java
+++ src/test/java/com/matteoveroni/javacopier/JavaCopierTest.java
@@ -11,7 +11,6 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.util.Arrays;
 
@@ -167,19 +166,51 @@ public class JavaCopierTest {
         CopyStatusReport copyStatusReport = JavaCopier.copy(srcDir.toPath(), destFile.toPath(), new FileOutputStream(destFile));
 
         assertTrue("Error, srcDir is not a directory", srcDir.isDirectory());
-        assertTrue("Error, destFile doesn't exist", destFile.exists());
-        assertTrue("Error, destFile is not a canonical file", destFile.isFile());
+        assertTrue("Error, destFile is not a canonical file", destFile.exists() && destFile.isFile());
+        assertTrue(copyStatusReport.getCopyHistory().getCopiedFiles().contains(srcDir.toPath().toAbsolutePath()));
+        assertTrue(copyStatusReport.getCopyHistory().getCopiedFiles().contains(srcFile1.toPath().toAbsolutePath()));
+        assertTrue(copyStatusReport.getCopyHistory().getCopiedFiles().contains(srcFile2.toPath().toAbsolutePath()));
         assertEquals(3, copyStatusReport.getNumberOfCopiedFiles());
         assertEquals(0, copyStatusReport.getNumberOfCopiesFailed());
         assertEquals(3, copyStatusReport.getCopyHistory().getCopiedFiles().size());
         assertEquals(0, copyStatusReport.getCopyHistory().getCopiesFailed().size());
-        assertTrue(copyStatusReport.getCopyHistory().getCopiedFiles().contains(Paths.get(String.valueOf(srcDir.toPath().toAbsolutePath()))));
-        assertTrue(copyStatusReport.getCopyHistory().getCopiedFiles().contains(Paths.get(String.valueOf(srcFile1.toPath().toAbsolutePath()))));
-        assertTrue(copyStatusReport.getCopyHistory().getCopiedFiles().contains(Paths.get(String.valueOf(srcFile2.toPath().toAbsolutePath()))));
         assertEquals(copyStatusReport.getFinalResult(), CopyStatusReport.FinalResult.COPY_SUCCESSFUL);
+
         srcFile1.delete();
         srcFile2.delete();
     }
+
+    @Test
+    public void copySrcDirIntoNotExistentDestDirWithReplaceCopyOption() throws IOException {
+        srcDir = new File("srcDir");
+        srcDir.mkdir();
+        File srcFile1 = new File(srcDir + File.separator + "srcFile1.pdf");
+        srcFile1.createNewFile();
+        File srcFile2 = new File(srcDir + File.separator + "srcFile2.zip");
+        srcFile2.createNewFile();
+        destDir = new File("destDir");
+
+        CopyStatusReport copyStatusReport = JavaCopier.copy(srcDir.toPath(), destDir.toPath(), StandardCopyOption.REPLACE_EXISTING);
+
+        assertTrue("Error, srcDir is not a directory", srcDir.isDirectory());
+        assertTrue("Error, destFile is not a canonical file", destDir.exists() && destDir.isDirectory());
+        assertTrue(copyStatusReport.getCopyHistory().getCopiedFiles().contains(srcDir.toPath().toAbsolutePath()));
+        assertTrue(copyStatusReport.getCopyHistory().getCopiedFiles().contains(srcFile1.toPath().toAbsolutePath()));
+        assertTrue(copyStatusReport.getCopyHistory().getCopiedFiles().contains(srcFile2.toPath().toAbsolutePath()));
+        assertEquals(3, copyStatusReport.getNumberOfCopiedFiles());
+        assertEquals(0, copyStatusReport.getNumberOfCopiesFailed());
+        assertEquals(3, copyStatusReport.getCopyHistory().getCopiedFiles().size());
+        assertEquals(0, copyStatusReport.getCopyHistory().getCopiesFailed().size());
+        assertEquals(copyStatusReport.getFinalResult(), CopyStatusReport.FinalResult.COPY_SUCCESSFUL);
+
+        srcFile1.delete();
+        srcFile2.delete();
+    }
+
+    private Path calculateDestPath(Path d, Path s, Path srcPath) {
+        return d.resolve(s.relativize(srcPath));
+    }
+
 
     private boolean isFileInsideDir(File file, File dir) throws IOException {
         File[] filesInDestDir = dir.listFiles();
